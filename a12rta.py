29cc4c5356cdbe99496af4853bcc03ab1bdddfc7
Pawel Suchanecki <subdcc@gmail.com>
2023-08-15 14:31:37 +0200

    run() remote execution error handling, writing to per host error log, etc

--- a12rta.py
+++ a12rta.py
@@ -7,6 +7,7 @@ import argparse
 import asyncio
 import datetime
 import time
+import os
 from asyncio.queues import Queue
 from fabric import Connection
 import yaml
@@ -16,24 +17,34 @@ async def producer(queue: Queue, host: dict):
     conn = Connection(
         host['host'],
         user=host['user'],
-        connect_kwargs={'key_filename': host['key_filename'], 'timeout': host['login_timeout']}
+        connect_kwargs={'key_filename': host['key_filename'], 'timeout': host.get('login_timeout', 10)}
     )
     with conn.cd('/tmp'):
         print(f"Connection to host {host['host']}, monitoring {host['log_file']}")  # Add this line
-        root_cmd = host.get('root_access_type', 'sudo')
-        if root_cmd not in ['sudo', 'doas']:
-            print(f"ERROR: Invalid root_access_type {root_cmd} for host {host['host']}. Supported values are 'sudo' or 'doas'.")
-            return
-        tail_cmd = f"{root_cmd} tail -n {host['buffer_lines']} {host['log_file']}"
+        tail_cmd = f"{host['root_access_type']} tail -n {host['buffer_lines']} {host['log_file']}"
         old_data = ()
         while True:
             try:
                 channel = conn.run(command=tail_cmd, hide='both')
             except TimeoutError:
-                print(f"ERROR: Connection to {host['host']} timed out.")
+                print(f"ERROR: Connection to {host['host']} timed out after {host.get('login_timeout', 10)} seconds.")
                 return
             except Exception as e:
-                print(f"ERROR: Error executing command on host {host['host']}: {e}")
+                if hasattr(e, 'result'):
+                    error_file_path = f"errors-{host['host']}.log"
+                    with open(error_file_path, 'a') as error_file:
+                        error_file.write(f"Timestamp: {datetime.datetime.now()}\n")
+                        error_file.write(f"Command: {e.result.command}\n")
+                        error_file.write(f"Exit code: {e.result.exited}\n")
+                        error_file.write(f"Stdout: {e.result.stdout}\n")
+                        error_file.write(f"Stderr: {e.result.stderr}\n\n")
+                    print(f"ERROR: Error executing command on host {host['host']}: Encountered a bad command exit code!")
+                    print(f" +-- TS: {datetime.datetime.now()}")
+                    print(f" +-- CMD: '{e.result.command}'")
+                    print(f" +-- EXIT CODE: {e.result.exited}")
+                    print(f" +-- LOG FILE: Error details written to {os.path.abspath(error_file_path)}")
+                else:
+                    print(f"ERROR: Error executing command on host {host['host']}: {e}")
                 return
 
             data = channel.stdout.splitlines()
